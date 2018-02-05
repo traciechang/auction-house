dfe46b6c3a10a2e16ee812a7c367dee08e7beabf
Tracie <traciechang@hotmail.com>
2018-02-05 12:47:36 -0800

    set up frontend

---
+++ frontend/auction_house.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDom.render(<h1>Welcome to Pusheen's Auction House</h1>, document.getElementById('root'));
+});
