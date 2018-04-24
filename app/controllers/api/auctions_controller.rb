5e2925e98df5a81fa123d8e407a9f8e1cd6008da
Tracie <traciechang@hotmail.com>
2018-04-23 21:45:40 -0700

    notes

--- app/controllers/api/auctions_controller.rb
+++ app/controllers/api/auctions_controller.rb
@@ -50,3 +50,68 @@ class Api::AuctionsController < ApplicationController
         params.slice(:item_name, :item_level_min, :item_level_max, :item_type, :item_quality)
     end
 end
+
+# ===
+
+# def index
+#     if params[:auc] == "myauctions"
+#         @auctions = current_user.auctions
+#     elsif params[:auc] == "mybids"
+#         auction_ids = current_user.bids.pluck(:auction_id).uniq
+#         @auctions = Auction.where(id: auction_ids)
+#     else
+#       @auctions = AuctionSearch.new(filter_params).search
+#         # @auctions = Auction.all
+
+#         # filtering_params(params).each do |key, val|
+#             # @auctions = @auctions.public_send(key, val) if val.present?
+#         # end
+#     end
+#     @auctions = @auctions.includes(inventory_item: :item).includes(:user).includes(bids: :user)
+# end
+
+# app/services/auction_search.rb
+
+# class AuctionSearch
+# def initialize(attributes)
+# @attributes = attributes
+# end
+
+# attr_reader :attributes
+
+# # def search
+# # auctions = Auction.all
+
+# # if attributes[:item_name]
+#   # auctions.with_item_name(attributes[:item_name])
+# # end
+
+# # if attributes[:item_level_min]
+#   # auctions.with_min_level(attributes[:item_name])
+# # end
+
+# # if attributes(:limit)
+#   # auctions.limit(attributes[:limit])
+# # end
+# # end
+
+# def search
+# items = Item.all
+
+# attributes.each do |column, val|
+#   items = items.where(column => val)
+# end
+
+# inventory_items = InventoryItem.where(item_id: items.ids)
+# Auction.where(inventory_item_id: inventory_items.ids)
+# end
+# end
+
+# scope naming suggestions for auction.rb
+
+# Auction.with_item_name('hammer')
+#  Auction.with_quality()
+#  Auction.with_minimum_level(level)
+#  Auction.with_max_level(level)
+#  Auction.with_item_type('weapon')
+#  Auction.ended
