3038d6e13a317f6dfd735ab3bbab4ce4c64e8875
Tracie <traciechang@hotmail.com>
2018-05-07 18:05:16 -0700

    fixed navbar flicker

--- frontend/components/navigation_bar/navigation_bar.jsx
+++ frontend/components/navigation_bar/navigation_bar.jsx
@@ -8,6 +8,12 @@ class NavigationBar extends React.Component {
         this.logout = this.props.logout.bind(this);
     }
 
+    componentDidMount() {
+        $('#navbarSupportedContent li').on('click', function() {
+            $('.navbar-collapse').collapse('hide');
+        });
+    }
+
     buttonDisplay() {
         if (this.props.loggedIn) {
             return (
@@ -37,10 +43,10 @@ class NavigationBar extends React.Component {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="nav navbar-nav d-flex">
                         <li class="p-2"><Link class="text-light" to="/">Browse</Link></li>
-                        <li class="p-2" data-toggle="" data-target="navbarSupportedContent"><Link class="text-light" to="sell">Sell</Link></li>
-                        <li class="p-2" data-toggle="collapse" data-target="#navbarSupportedContent"><Link class="text-light" to="bids">My Bids</Link></li>
-                        <li class="p-2" data-toggle="collapse" data-target="#navbarSupportedContent"><Link class="text-light" to="auctions">My Auctions</Link></li>
-                        <li class="p-2" data-toggle="collapse" data-target="#navbarSupportedContent"><Link class="text-light" to="profile">Profile</Link></li>
+                        <li class="p-2"><Link class="text-light" to="sell">Sell</Link></li>
+                        <li class="p-2"><Link class="text-light" to="bids">My Bids</Link></li>
+                        <li class="p-2"><Link class="text-light" to="auctions">My Auctions</Link></li>
+                        <li class="p-2"><Link class="text-light" to="profile">Profile</Link></li>
 
                         {this.buttonDisplay()}
                     </ul>
